d428db5e2f0aed5313f0de933ca876f39901ff03
Djonatan Abreu <djonatan@Djonatans-MacBook-Air.local>
2023-04-09 04:48:55 -0300

    feat: Added RTK Query to connect requests search with Api

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -4,6 +4,9 @@ import { setSearch } from '@/store/searchSlice';
 import { AppDispatch, RootState } from '@/store/store';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { PokemonTable } from '../PokemonTable/PokemonTable';
+import { IPokemonProps } from '@/types';
+import { useEffect } from 'react';
+import { pokemonApi } from '@/store/pokemonApi';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 
@@ -12,6 +15,14 @@ export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const SearchInput = () => {
   const dispatch = useAppDispatch();
   const { search, startupPokemon } = useAppSelector((state) => state.search);
+  const data = useAppSelector(
+    (state) =>
+      state.pokemonApi.queries[`search("${search}")`]?.data as IPokemonProps[]
+  );
+
+  useEffect(() => {
+    dispatch(pokemonApi.endpoints.search.initiate(search));
+  }, [dispatch, search]);
 
   return (
     <div>
@@ -20,7 +31,7 @@ export const SearchInput = () => {
         value={search}
         onChange={({ target: { value } }) => dispatch(setSearch(value))}
       />
-      <PokemonTable pokemons={startupPokemon} />
+      <PokemonTable pokemons={search.length ? data ?? [] : startupPokemon} />
     </div>
   );
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchSlice from './searchSlice';
+import { pokemonApi } from './pokemonApi';
 
 export const store = configureStore({
   reducer: {
     search: searchSlice,
+    pokemonApi: pokemonApi.reducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware().concat(pokemonApi.middleware);
   },
 });
 
